ba68c04623423e4f65a9057ef3667ff33094532c
Timur Morozov <radist@desant.biz>
2013-04-16 21:32:51 +0300

    layout sample

--- app/assets/javascripts/welcome/layout.js
+++ app/assets/javascripts/welcome/layout.js
@@ -1,17 +1,31 @@
 app.module("welcome", function(mod, app){
 
   mod.on("hello:next", function () {
-    app.vent.trigger("show:next", mod.mainRegion);
+    app.vent.trigger("show:next", mod.layout.userRegion);
   });
+
+  mod.AppLayout = Backbone.Marionette.Layout.extend({
+    template: "#welcome-layout-template",
+
+    regions: {
+      userRegion: "#user-data",
+      selectorRegion: "#user-selector"
+    }
+  });
+
 
   mod.addInitializer(function (options) {
     // body...
     console.log("welcome module selector: ", options.region_selector);
+
     mod.mainRegion = new Backbone.Marionette.Region({
       el: options.region_selector
     });
 
-    app.vent.trigger("show:hello", mod.mainRegion);
+    mod.layout = new mod.AppLayout();
+    mod.mainRegion.show(mod.layout);
+
+    app.vent.trigger("show:hello", mod.layout.userRegion);
   });
 
 });
